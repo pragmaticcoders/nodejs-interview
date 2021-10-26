974e622e8f52fb83beb86ba92f32e90497664cc2
Dominik Kroliczek <dominik@kroliczek.com>
2021-10-26 12:54:18 +0200

    Remove class validator unused deps

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -1,6 +1,3 @@
-import { ValidationError } from "class-validator";
-import { isNil } from "ramda";
-
 export type HttpErrorResponseBody = { message: string; kind?: string };
 
 export class HttpErrorResponse<
@@ -11,37 +8,3 @@ export class HttpErrorResponse<
     Object.setPrototypeOf(this, HttpErrorResponse.prototype);
   }
 }
-
-export class TransformError extends Error {
-  constructor(readonly message: string, readonly validationErrors: ValidationError[]) {
-    super(message);
-    Object.setPrototypeOf(this, TransformError.prototype);
-  }
-}
-
-function getChildrenErrors(parentErrors: ValidationError[], propPath: string) {
-  let acc: ValidationError[] = [];
-  for (const error of parentErrors) {
-    if (error.constraints) {
-      acc = acc.concat({ ...error, property: `${propPath}.${error.property}` });
-    }
-    if (error.children.length) {
-      acc = acc.concat(getChildrenErrors(error.children, `${propPath}.${error.property}`));
-    }
-  }
-  return acc;
-}
-
-export const parseValidationErrors = (validationErrors: ValidationError[]): string => {
-  const acc = getChildrenErrors(validationErrors, "");
-
-  return acc
-    .map(error => {
-      if (isNil(error.constraints)) {
-        return "";
-      } else {
-        return `${Object.values(error.constraints).join(", ")}`;
-      }
-    })
-    .join(";");
-};
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,3 @@
-export * from "./validation";
 export * from "./logger";
 export * from "./common";
 export * from "./errors";
